d15a4ed2dc22887cd88135c18780416c324ba5d2
Eshem Waseem <eshemwaseem111@gmail.com>
2026-07-27 23:28:59 +0500

    app fixed

--- backend/app.py
+++ backend/app.py
@@ -17,7 +17,7 @@ origins = [
     "http://localhost:3000",
     "http://localhost:5500",
     "http://127.0.0.1:5500",
-    "https://task-manager-seven-steel.vercel.app"
+    "https://task-manager-1a8h-kv18eioxd-eshem-waseem.vercel.app/"
 ]
 
 app.add_middleware(
